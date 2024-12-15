35ecafaa286d5d5dd64319263ab7061d1181c77e
Royusiwu <royussiwu00@gmail.com>
2024-12-16 04:04:55 +0800

    Initial commit with my folder's files

--- app.py
+++ app.py
@@ -1,78 +1,45 @@
-import streamlit as st 
-# ML Pkg
-# from sklearn.externals import joblib
-import joblib
-import time
-from PIL import Image
+from flask import Flask,render_template,url_for,request
+from flask_bootstrap import Bootstrap 
+import pandas as pd 
+import numpy as np 
 
-# load Vectorizer For Gender Prediction
-gender_vectorizer = open("models/gender_vectorizer.pkl","rb")
-gender_cv = joblib.load(gender_vectorizer)
-
-# load Model For Gender Prediction
-gender_nv_model = open("models/naivebayesgendermodel.pkl","rb")
-gender_clf = joblib.load(gender_nv_model)
+# ML Packages
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.externals import joblib
 
 
-# Prediction
-def predict_gender(data):
-	vect = gender_cv.transform(data).toarray()
-	result = gender_clf.predict(vect)
-	return result
-
-# Load CSS File
-def load_css(file_name):
-    with open(file_name) as f:
-        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
-
-def load_icon(icon_name):
-    st.markdown('<i class="material-icons">{}</i>'.format(icon_name), unsafe_allow_html=True)
-
-# Images
-def load_images(file_name):
-	img = Image.open(file_name)
-	return st.image(img,width=300)
+app = Flask(__name__)
+Bootstrap(app)
 
 
-def main():
-	"""Gender Classifier App
-	Using Machine Learning and Streamlit
+@app.route('/')
+def index():
+	return render_template('index.html')
 
-	"""
+@app.route('/predict', methods=['POST'])
+def predict():
+	df= pd.read_csv("data/names_dataset.csv")
+	# Features and Labels
+	df_X = df.name
+	df_Y = df.sex
+    
+    # Vectorization
+	corpus = df_X
+	cv = CountVectorizer()
+	X = cv.fit_transform(corpus) 
+	
+	# Loading our ML Model
+	naivebayes_model = open("models/naivebayesgendermodel.pkl","rb")
+	clf = joblib.load(naivebayes_model)
 
-	st.title("Gender Classifier")
-	html_temp = """
-	<div style="background-color:tomato;padding:10px">
-	<h2 style="color:white;text-align:center;">Streamlit ML App </h2>
-	</div>
-
-	"""
-	st.markdown(html_temp,unsafe_allow_html=True)
-	load_css('icon.css')
-	load_icon('people')
-
-	name = st.text_input("Enter Name","Type Here")
-	if st.button("Predict"):
-		result = predict_gender([name])
-		if result[0] == 0:
-			prediction = 'Female'
-			c_img = 'female.png'
-		else:
-			result[0] == 1
-			prediction = 'Male'
-			c_img = 'male.png'
-
-		st.success('Name: {} was classified as {}'.format(name.title(),prediction))
-		load_images(c_img)
-
-
-	if st.button("About"):
-
-		st.text("Jesus Saves @JCharisTech")
-		st.text("By Jesse E.Agbe(JCharis)")
-		st.text("Built with Streamlit")
-
+	# Receives the input query from form
+	if request.method == 'POST':
+		namequery = request.form['namequery']
+		data = [namequery]
+		vect = cv.transform(data).toarray()
+		my_prediction = clf.predict(vect)
+	return render_template('results.html',prediction = my_prediction,name = namequery.upper())
 
 
 if __name__ == '__main__':
-	main()
+	app.run(debug=True)
